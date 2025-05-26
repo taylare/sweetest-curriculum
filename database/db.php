3cc66ce7d091b4b661709f9cf0dd4724d7a494ee
Tayla Rechichi <tay.rechichi32@gmail.com>
2025-05-25 17:44:33 -0700

    setup database config files

---
+++ database/db.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$hostname = '127.0.0.1';
+$username = 'ICS199Grp02_Dev';
+$pw = '567890_Dev';
+$db = 'ICS199Grp02_DevDB';
+
+$dbc = mysqli_connect($hostname, $username, $pw, $db);
+
+$sql = 'select user_id, username, userEmail from users;';
+$result = mysqli_query($dbc, $sql);
+
+if (!$dbc) {
+	die (mysqli_connect_error());
+} else {
+	print "db connected" . $db;
+}
+
+if($result){
+    while($row = mysqli_fetch_array($result)){
+	
+        print "<br>" . "user id: " . $row['user_id'] . ", " . $row['username'] . " " . $row['userEmail'] . "<br>";
+    }
+}
+
+?>
+
