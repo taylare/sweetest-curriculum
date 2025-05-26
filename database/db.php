a4cc8f92d44b1fb6d34355b2e1720d64af484315
Tayla Rechichi <tay.rechichi32@gmail.com>
2025-05-26 10:55:06 -0700

    changed db.php file to connect to local db

--- database/db.php
+++ database/db.php
@@ -1,9 +1,9 @@
 <?php
 
 $hostname = '127.0.0.1';
-$username = 'ICS199Grp02_Dev';
-$pw = '567890_Dev';
-$db = 'ICS199Grp02_DevDB';
+$username = 'root'; //'ICS199Grp02_Dev';
+$pw = ''; //'567890_Dev';
+$db = 'sweetest-curriculum'; //'ICS199Grp02_DevDB';
 $dbc = mysqli_connect($hostname, $username, $pw, $db);
 $sql = 'select user_id, username, userEmail from users;';
 $result = mysqli_query($dbc, $sql);
