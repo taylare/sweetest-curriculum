880381b4c4bdd9227cd23f25daecaca54da51b56
Tayla Rechichi <tay.rechichi32@gmail.com>
2025-05-30 10:34:21 -0700

    fixed remove-products functionality, updated delete queries to solve FK constraints

--- admin/remove-product.php
+++ admin/remove-product.php
@@ -9,17 +9,20 @@
 
     $id = (int)$_GET['id']; // sanitize input
 
-    //delete product from cart
-    $sql = "DELETE FROM cart WHERE product_id = $id;";
-    mysqli_query($dbc, $sql);
+    // delete related reviews
+    mysqli_query($dbc, "DELETE FROM reviews WHERE product_id = $id");
 
-    //delete from product_category
-    $sql = "DELETE FROM product_category WHERE product_id = $id;";
-    mysqli_query($dbc, $sql);
+    // delete from order_items
+    mysqli_query($dbc, "DELETE FROM order_items WHERE product_id = $id");
 
-    //delete from products
-    $sql = "DELETE FROM products WHERE product_id = $id;";
-    $result = mysqli_query($dbc, $sql);
+    // delete from cart
+    mysqli_query($dbc, "DELETE FROM cart WHERE product_id = $id");
+
+    // delete from product_category
+    mysqli_query($dbc, "DELETE FROM product_category WHERE product_id = $id");
+
+    // finallyyy, delete from products
+    $result = mysqli_query($dbc, "DELETE FROM products WHERE product_id = $id");
 
     if ($result) {
         header("Location: dashboard.php");
