208ffb0ec3fe9d980f297fc856b5051d26741c4b
krambolo <mayet.escobartaborda@gmail.com>
2025-05-30 11:56:44 -0700

    added nav & footer files

---
+++ includes/footer.php
@@ -0,0 +1,36 @@
+    <footer>
+      <br>
+      <h5>Store hours:</h5>
+      <p>Mon : closed<br>
+      Tue-Thur : 10am-6pm<br>
+      Fri - Sun : 9am-7pm</p>
+
+      <br>
+
+      <p>710 Douglas St<br>
+      Victoria BC<br>
+      +1 (250) 084-7126</p>
+
+      <br>
+
+      <p id="connect">Connect with us!</p>
+      <div class="icons d-flex justify-content-center">
+        
+        <ul class="list-group list-group-horizontal p-2">
+          <li class="icon_list p-1 list-unstyled"><a href='https://www.instagram.com/' target=”_blank”><i class="fa-brands fa-instagram"></i></a></li>
+          <li class="icon_list p-1 list-unstyled"><a href='https://www.facebook.com/' target=”_blank”></a><i class="fa-brands fa-facebook"></i></li>
+          <li class="icon_list p-1 list-unstyled"><a href='https://www.tiktok.com/explore' target=”_blank”></a><i class="fa-brands fa-tiktok"></i></li>
+        </ul>
+      </div>
+      <p>info@thesweetestcurriculum.ca</p>
+      
+      Copywright &copy; 2025 The Sweetest Curriculum
+    </footer>
+    <!-- body content end -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js">/*! jQuery v3.7.1 | (c) OpenJS Foundation and other contributors | jquery.org/license */</script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="includes/script.js"></script>
+  </body>
+
+
+</html>
