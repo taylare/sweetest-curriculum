ee48ee160cd84f09924378d829ed867f5d6f5d6a
C0527796 <tay.rechichi32@gmail.com>
2025-05-30 20:20:34 +0000

    Edit index.php

--- index.php
+++ index.php
@@ -10,7 +10,10 @@ include 'database/db.php';
             $user = htmlspecialchars($_SESSION['username']);
             echo $user . "! <br>";
         }?></h1>
-
-<a href="login.php">Login</a>
+    <?php if(isset($_SESSION['logged-in']) && $_SESSION['logged-in']) { ?>
+        <a href="logout.php">Logout</a>
+    <?php  } else {?>
+        <a href="login.php">Login</a>
+    <?php } ?>
 <br>
 <a href="store.php">View our store</a>
