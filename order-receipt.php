0361f262f3f7f67ec93c451e4aa1330786591ac6
Brett <bretttacrowley@gmail.com>
2025-06-20 05:13:10 -0700

    Basis of receipt is there, stylesheet just needs some love. sorry but haven't gotten a file created with the order yet

--- order-receipt.php
+++ order-receipt.php
@@ -1,3 +1,4 @@
+
 <?php
   include 'includes/header.php';
   include 'database/db.php';
@@ -21,11 +22,6 @@
       'currency' => 'cad'
   ));
   // --------------- end of background stripe stuff ---------------
-  
-
-  // $amount = number_format(($totalamt / 100), 2);
-  //echo '<h3>Successfully charged $'.$amount.' </h3>Thank you for shopping at My Simple Shopping Cart';
-
 ?>
 
 <?php 
@@ -62,8 +58,8 @@ mysqli_query($dbc, $insertItems);
 //-----------------------------------//
 //-----------clear the cart----------//
 //----------------------------------//
-$clearCart = "DELETE FROM cart WHERE user_id = $user_id";
-mysqli_query($dbc, $clearCart);
+// $clearCart = "DELETE FROM cart WHERE user_id = $user_id";
+// mysqli_query($dbc, $clearCart);
 
 
 //-------------------------------------//
@@ -74,6 +70,8 @@ $sqlReceipt = "SELECT
                 oh.order_id,
                 u.username AS customer_name,
                 p.productName,
+                p.product_id,
+                p.imageURL,
                 oi.quantity,
                 oi.price,
                 (oi.quantity * oi.price) AS total_price_per_item,
@@ -93,6 +91,9 @@ $orderID = null;
 $user = null;
 $products = [];
 
+// set an empty variable to store the total order cost in
+$totalOrderCost = 0;
+
 if (mysqli_num_rows($receiptResult) > 0) {
   while ($row = mysqli_fetch_assoc($receiptResult)) {
     if (!$orderID) $orderID = $row['order_id'];
@@ -100,24 +101,61 @@ if (mysqli_num_rows($receiptResult) > 0) {
 
     $products[] = [
       'name' => $row['productName'],
-      'qty' => $row['quantity']
+      'qty' => $row['quantity'],
+      'pid' => $row['product_id'],
+      'price' => $row['price'],
+      'image' => $row['imageURL'],
     ];
+    $totalOrderCost += $row['price'];
   }
 }
 
   ?>
-
-<p>Order Successful yaaay</p>
-<p>Order id: <?= htmlspecialchars($orderID) ?></p>
-<p>Username: <?= htmlspecialchars($user) ?></p>
-<p>Products purchased:</p>
-<ul>
-  <?php foreach ($products as $item): ?>
-    <li><?= htmlspecialchars($item['qty']) ?> × <?= htmlspecialchars($item['name']) ?></li>
-  <?php endforeach; ?>
-</ul>
-
-
-
-
+<table id="order-receipt-container">
+  <form id="order-receipt-form">
+    <!-- Thank you message (head of receipt) -->
+    <tr>
+      <td id="order-receipt-confirmation"> <h3> Your Order Has Been Confirmed! </h3> </td>
+    </tr>
+    <!-- Order ID (head of receipt) -->
+    <tr>
+      <td id="order-receipt-order-id"> <p>Order id: <?= htmlspecialchars($orderID) ?></p> </td>
+    </tr>
+    <!-- Show username of customer (sub head)--> 
+    <tr>
+      <td id="order-receipt-username"> <p>Username: <?= htmlspecialchars($user) ?></p> </td>
+    </tr>
+     <!-- Show Day and time order was placed (sub head)--> 
+    <tr>
+      <?php
+        $dateResult = mysqli_query($dbc, "SELECT created_at FROM order_history WHERE order_id = $orderID");
+        $dateRow = mysqli_fetch_assoc($dateResult);
+        $date = $dateRow['created_at'];
+      ?>
+      <td id="order-receipt-date"> <p>Order Placed: <?= htmlspecialchars($date) ?></p> </td>
+    </tr>
+      <?php foreach ($products as $item): ?>
+        <tr>
+          <!-- Display Product Image -->
+          <div id="order-receipt-image-container">
+            <td id="order-receipt-image"> <img src="assets/images/<?= htmlspecialchars($item['image'])?>" alt="<?= htmlspecialchars($item['name'])?> Macaron"> </td>
+          </div>
+          <!-- Display Product name -->
+          <td id="order-receipt-prodname"> <h3> <?= htmlspecialchars($item['name'])?></h3> </td>
+          <!-- Display Product Price -->
+          <td id="order-receipt-product-price"> <h3> price: $<?= htmlspecialchars($item['price'])?> </h3> </td>
+          <!-- Display Product quantity -->
+          <td id="order-receipt-item-qty"> <h3> Ordered: <?= htmlspecialchars($item['qty'])?> </h3> </td>
+          <!-- Display Product total -->
+          <td id="order-receipt-item-total"> <h3> Total: $<?= number_format((htmlspecialchars($item['qty']) * htmlspecialchars($item['price'])), 2)?> </h3> </td>
+        </tr>
+      <?php endforeach; ?>
+      <!-- Show Total Order Cost (Bottom) --> 
+    <tr>
+      <?php $amount = number_format(($totalamt / 100.00), 2); ?>
+      <td id="order-receipt-total"> <h3> Order Total: $<?=htmlspecialchars($amount)?> </h3> </td>
+    </tr>
+  </form>
+</table>
+  
 <?php include 'includes/footer.php'; ?>
